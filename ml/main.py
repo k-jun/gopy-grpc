bac21f5bf4117a551fd17cac0b61d1c2b82636ff
K-jun <poittyo091221@gmail.com>
2019-09-03 00:25:34 +0900

    feat: add simple nn sample

--- ml/main.py
+++ ml/main.py
@@ -4,20 +4,44 @@ import time
 import math
 import logging
 
+import json
+
 import grpc
 import protolib
 import socket
+
+from tensorflow.keras.models import model_from_json
+import numpy as np
 
 ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
 class RouteGuideServicer:
+    def __init__(self):
+        # In tensorflow 1.14.0, Got error: call initializer instance with the dtype argument instead of passing it to the constructor.
+        # Need tensorflow 2.0.0rc0 to avoid it.
+        super().__init__()
+        with open("./models/simple_nn.json", "r") as f:
+            json_string = json.load(f)
+
+        self.model = model_from_json(json_string)
+        self.model.load_weights("./models/simple_nn_weights.h5")
+
     def Predict(self, request, context):
-        print(request)
-        print(context)
-        host = socket.gethostname()
-        ip = socket.gethostbyname(host)
-        return protolib.Response(irisType=str("host: " + host + " ip: " + ip))
+        # TODO Irisのコードを差し込む
+        input = [
+            request.sepalLength,
+            request.sepalWidth,
+            request.petalLength,
+            request.petalWidth,
+        ]
+
+        output = np.argmax(self.model.predict(np.array([input])))
+        labels = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
+
+        # host = socket.gethostname()
+        # ip = socket.gethostbyname(host)
+        return protolib.Response(irisType=labels[output])
 
 
 def serve():
--- server/interface/controllers/predict_controllers.go
+++ server/interface/controllers/predict_controllers.go
@@ -11,10 +11,10 @@ import (
 
 func PredictIris(c echo.Context) error {
 	arg := &adtech.Request{
-		SepalLength: 1.0,  // not 0.0
-		SepalWidth:  1.0,  // not 0.0
-		PetalLength: 0.5,  // not 0.0
-		PetalWidth:  10.0, // not 0.0
+		SepalLength: 4.8, // not 0.0
+		SepalWidth:  3.0, // not 0.0
+		PetalLength: 1.4, // not 0.0
+		PetalWidth:  0.1, // not 0.0
 	}
 	res, err := grpc.Predict(arg)
 
